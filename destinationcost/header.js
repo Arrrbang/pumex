3327b706cb9ec44262ace7ae423471fadd522885
Arrrbang <arem0602@gmail.com>
2025-11-14 21:53:42 +0900

    if문 변경

--- destinationcost/header.js
+++ destinationcost/header.js
@@ -8,14 +8,14 @@ const TRCMenu = document.getElementById("menu-TRC");
 const consoleMenu = document.getElementById("menu-console");
 
 // 연결될 URL
-const destinationURL = "https://arrrbang.github.io/pumex/destinationcost";
+const destinationURL = "https://arrrbang.github.io/pumex/destinationcost/index.html";
 const pumexsosURL = "추후입력";
 const provincial_packing_feeURL = "추후입력";
 const TRCURL = "추후입력";
 const consoleURL = "추후입력";
 
 // 현재 URL과 비교하여 활성화
-if (currentURL === destinationURL) destinationMenu.classList.add("always-on");
+if (currentURL.startsWith(destinationURL)) {destinationMenu.classList.add("always-on");}
 if (currentURL === pumexsosURL) pumexsosMenu.classList.add("always-on");
 if (currentURL === provincial_packing_feeURL) provincial_packing_feeMenu.classList.add("always-on");
 if (currentURL === TRCURL) TRCMenu.classList.add("always-on");
