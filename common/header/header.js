92828b82d7126904de7f376e08754e69f1376dfb
Arrrbang <arem0602@gmail.com>
2025-11-16 22:48:55 +0900

    Update header.js

--- common/header/header.js
+++ common/header/header.js
@@ -4,19 +4,19 @@ const currentURL = window.location.href;
 const destinationMenu = document.getElementById("menu-destination");
 const pumexsosMenu = document.getElementById("menu-sos");
 const provincial_packing_feeMenu = document.getElementById("menu-provincial_packing_fee");
-const TRCMenu = document.getElementById("menu-TRC");
+const trcMenu = document.getElementById("menu-TRC");
 const consoleMenu = document.getElementById("menu-console");
 
 // 연결될 URL
 const destinationURL = "https://arrrbang.github.io/pumex/destinationcost/";
 const pumexsosURL = "https://arrrbang.github.io/pumex/sos/";
 const provincial_packing_feeURL = "추후입력";
-const TRCURL = "추후입력";
+const trcURL = "추후입력";
 const consoleURL = "추후입력";
 
 // 현재 URL과 비교하여 활성화
 if (currentURL.startsWith(destinationURL)) {destinationMenu.classList.add("always-on");}
 if (currentURL.startsWith(pumexsosURL)) {pumexsosMenu.classList.add("always-on");}
 if (currentURL === provincial_packing_feeURL) provincial_packing_feeMenu.classList.add("always-on");
-if (currentURL === TRCURL) TRCMenu.classList.add("always-on");
+if (currentURL === trcURL) trcMenu.classList.add("always-on");
 if (currentURL === consoleURL) consoleMenu.classList.add("always-on");
